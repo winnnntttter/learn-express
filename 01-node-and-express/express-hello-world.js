d3b1c0d90f4dc2e059487c75fb6d6b9b417da987
winnnntttter <1013222027@qq.com>
2018-01-23 09:53:47 +0800

    修改git用户

---
+++ 01-node-and-express/express-hello-world.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+  //res.type('text/plain');
+  res.send('Hello World');//或者{'msg':'hello world'},此时默认响应头的Content-Type为application/json;
+});
+app.listen(3000);
+
+console.log('Server is running on localhost:3000;press ctrl+c to stop.');
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 - git init生成本地库
 - 将node_modules添加到.gitignore
-- 
+- git add -A
+- git commit -m ""
+- git remote add origin https://github.com/winnnntttter/learn-express.git
+- git push -u origin master
