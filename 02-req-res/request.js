06b344834f0bace98aed9e4e5a17056f5015fa84
winnnntttter <1013222027@qq.com>
2018-04-27 15:41:46 +0800

    req和res的进一步理解

--- 02-req-res/request.js
+++ 02-req-res/request.js
@@ -5,6 +5,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));//不添加则表单提交的数据无效
 
 const staff = {
   beijing:{
@@ -16,6 +17,20 @@ const staff = {
   }
 };
 
+const staff2 = [
+  {
+    city:"beijing",
+    name:"li",
+    like:"reading"
+  },
+  {
+    city:"beijing",
+    name:"wang",
+    like:"writing"
+  }
+];
+
+
 app.get('/staff/:city/:name',(req,res,next)=>{
   console.log(req.params);//{ city: 'beijing', name: 'li' }
 
@@ -23,24 +38,51 @@ app.get('/staff/:city/:name',(req,res,next)=>{
 
   //console.log(req.route);
 
-  let like = staff[req.params.city][req.params.name].like;
-  if(!like){
-    console.log("ooh,not found");
-    return next();
+  if(!staff[req.params.city]){
+    console.log("ooh, city not found");
+    res.send("ooh, city not found");
+    //return next();
+  }else if(!staff[req.params.city][req.params.name]){
+    console.log("ooh, people not found");
+    res.send("ooh, people not found");
+    //return next();
+  }else{
+    let like = staff[req.params.city][req.params.name].like;
+    res.send(`${req.params.name} form ${req.params.city} like ${like}`);
   }
-  res.send(`${req.params.name} form ${req.params.city} like ${like}`);
 });
 
-app.post('/staff', (req, res,next) => {
+app.get('/staff',(req, res, next)=>{
+  res.type('text/html');
+  res.send('<form action="/staff" method="post"><input name="beijing"><button type="submit">提交</button></form>');
+});
+
+app.post('/staff', (req, res, next) => {
+  console.log(req.body);
+
   for(let i in req.body){
+    const jsonTemp = JSON.parse(req.body[i]);//传json格式value
     if(staff[i]){
-      Object.assign(staff[i],req.body[i]);
+      Object.assign(staff[i],jsonTemp);
     }else{
-      staff[i] = req.body[i];
+      staff[i] = jsonTemp;
     }
   }
-  console.log(staff);
   res.json(staff);
+});
+
+app.get('/staff2',(req, res, next)=>{
+  res.type('text/html');
+  res.send('<form action="/staff2" method="post">城市：<input name="staff[0][city]">姓名：<input name="staff[0][name]">爱好：<input name="staff[0][like]"><br>城市：<input name="staff[1][city]">姓名：<input name="staff[1][name]">爱好：<input name="staff[1][like]"><br><button type="submit">提交</button></form>');
+});//extended需设置为true
+
+app.post('/staff2', (req, res, next) => {
+  console.log(req.body);
+  //staff2 = staff2.concat(req.body.staff);
+  for(let i in req.body.staff){
+    staff2.push(req.body.staff[i]);
+  }
+  res.json(staff2);
 });
 
 app.use((req,res,next)=>{
