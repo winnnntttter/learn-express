f544854f92f6b87c7d3b1b7658d46f659f376dd0
winnnntttter <1013222027@qq.com>
2018-05-17 17:51:03 +0800

    一些测试

--- 02-req-res/test.sh
+++ 02-req-res/test.sh
@@ -2,7 +2,7 @@ curl "http://localhost:3000/staff/beijing/li?aa=1&bb=2"
 sleep 1
 
 # 测试json提交
-curl -H 'Content-Type: application/json' -X POST -d '{"beijing":{"zhao":{"like":"listening"}}}' "http://localhost:3000/staff" -i 
+curl -H 'Content-Type: application/json' -X POST -d '{"paris":{"jojo":{"like":"fighting"}}}' "http://localhost:3000/staff" -i 
 
 curl -H 'Content-Type: application/json' -X POST -d '{"beijing":{"zhao":{"like":"listening"},"zhang":{"like":"playing"}}}' "http://localhost:3000/staff" -i 
 
